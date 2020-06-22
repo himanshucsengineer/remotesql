0f5f57f5ff9805d05d1ba920144f4e703301f340
Parag <paragikjain786@gmail.com>
2020-06-22 18:24:25 +0530

    adding seprate header and footer and proper indenation and page linking and bit desing changes

--- dashboard.php
+++ dashboard.php
@@ -14,91 +14,7 @@
     </head>
     <body>
 
-      <header class=" head">
-
-        
-    
-        <div class="container">
-
-            <div class="row">
-
-                <div class="col-lg-12">
-
-                    <nav class="navbar navbar-expand-lg">
-
-                        <div class="logo">
-
-                           <a href="#"  rel="home"><img width="133" height="55" src="assest/img/logo.png"  alt="RemoteSql" /></a>
-                                
-
-                                      
-
-                                </a>
-
-                                
-                        </div>
-
-                        
-
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
-                            <i class="fa fa-bars" style="color: white;"></i>
-
-                            
-
-                        </button>
-
-
-
-                        <div id="navbarSupportedContent" class="collapse navbar-collapse sub-menu-bar text-center">
-                            <ul id="nav" class="navbar-nav ml-auto">
-                                <li  class=" active nav-item">
-                                    <a title="Home" href="#" class="nav-link">
-                                        Home
-                                    </a>
-                                </li>
-                                <li class=" nav-item" >
-                                    <a title="Tutorial" href="#" class="nav-link">
-                                        Tutorial
-                                    </a>
-                                </li>
-                                <li  class=" nav-item" > 
-                                    <a title="FAQ" href="#" class="nav-link">
-                                        FAQ
-                                    </a>
-                                    
-                                </li>
-                                <li class=" nav-item">
-                                    <a title="About us" href="#" class="nav-link">
-                                        About us
-                                    </a>
-                                </li>
-                                <li  class=" nav-item">
-                                    <a title="login/register" href="login.html" class="nav-link">
-                                        login/register
-                                    </a>
-                                
-                                </li>
-
-</ul></div>
-
-
-
-
-
-
-                    </nav> <!-- navbar -->
-
-                </div>
-
-            </div> <!-- row -->
-
-        </div> <!-- container -->
-
-  <!-- navbar area -->
-
-</header> 
-
+    <?php include('common/header.php');?>
         <div class="ad">
         <div class="container table-responsive">
             <h3>Account Details</h3>    
@@ -184,22 +100,6 @@
             </div>    
             
         </div>    
-        
-        <footer class="last">
-            <div class="container">
-                <p>follow us on</p>
-                
-                  
-                    <div class="text-center">
-                    <a href="#"><i class="fa fa-facebook"></i></a>
-                    <a href="#"><i class="fa fa-youtube"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
-                    <a href="#"><i class="fa fa-linkedin"></i></a>
-                    </div>
-                 
-                
-                <p class="reserved ">@2020 Eniacoder All Rights Reserved. Developed By <span><a href="https://eniacoder.com/" class="lik">Eniacoder</a></span></p>
-              </div>
-        </footer>    
-        </body>
+        <?php include('common/footer.php');?>
+      </body>
 </html>
