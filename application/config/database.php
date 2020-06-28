ec7dd173abe22c6b62b2a535c62e49deb8efcdd7
Parag <paragikjain786@gmail.com>
2020-06-28 09:59:12 +0530

    folder structure

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '@Parag1',
+	'database' => 'remotesql',
 	'dbdriver' => '',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
